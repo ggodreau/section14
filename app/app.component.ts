7da69647ed5e4c773d61f0c8e41752165dbefd9e
ggodreau <godreau@gmail.com>
2017-06-12 22:51:26 +0100

    174 completed - parameterized routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'my-app',
   template: `
-    <h1>My First Angular 2 App</h1>
+    <h1>App</h1>
     <messages></messages>
     <ul>
         <li><a routerLink="">Home</a></li>
         <li><a routerLink="messages">Messages</a></li>
         <li><a routerLink="photos">Photos</a></li>
+        <!-- this passes a parameter string -->
+        <li><a [routerLink]="['photos', 1]">Photo Details</a></li>
     </ul>
+    <!-- this is an example of imperative routing -->
+    <button (click)="onClick()">Click Me!</button>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent { }
+export class AppComponent { 
+
+    constructor(private _router: Router){
+    }
+
+    onClick(){
+        console.log("clicked!");
+        this._router.navigate(['photos', 2]);
+    }
+
+
+}
--- app/photos/photo-details.component.ts
+++ app/photos/photo-details.component.ts
@@ -1,9 +1,32 @@
-
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-    template: `<h1>Photo Details</h1>
+template: `<h1>Photo Details, ID = {{ id }}</h1>
     `
 })
-export class PhotoDetailsComponent {
+export class PhotoDetailsComponent implements OnInit, OnDestroy {
+    id;
+    subscription;
+
+    constructor(private _route: ActivatedRoute){
+    }
+
+    ngOnInit(){
+    // note that the plus sign converts string to int
+        this.subscription = this._route.params.subscribe(params => {
+            this.id = +params["id"];
+        });
+
+        // Alternate method which won't reload the component
+        // on subsequent clicks
+        //this.id = this._route.snapshot.params["id"];
+    }
+
+    // this is used to clean up variables, which are 
+    // available outside the component after the page terminates
+    ngOnDestroy(){
+        this.subscription.unsubscribe();
+    }
+
 }
